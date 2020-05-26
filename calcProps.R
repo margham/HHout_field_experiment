fbbaccd39d329c2597061bd74b6f70dea5093952
unknown <james.weedon@vu.nl>
2020-05-26 20:39:07 +0200

    bugfix and progress

--- calcProps.R
+++ calcProps.R
@@ -1,8 +1,9 @@
 calcProp <- function(mat){
   output <- matrix(NA, nrow = nrow(mat), ncol = ncol(mat)+1)
   for(i in 1:ncol(mat)){
-    output[,i] <- exp(mat[,i])/(rowSums(exp(mat)+1))
+    output[,i] <- exp(mat[,i])/(rowSums(exp(mat))+1)
   }
   output[,ncol(mat)+1] <- 1 - rowSums(output[,1:ncol(mat)])
-  return(output)
+  newoutput <- cbind(output[,ncol(mat)+1], output[,1:ncol(mat)])
+  return(newoutput)
 }
